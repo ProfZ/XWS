01200985694b9cb57e8d2a5f177a71bb8d96b19a
SashaxDx <alexandraaleksic@hotmail.com>
2015-06-20 16:54:29 +0200

    Implementacija REST poziva i poziv odgovarajucis metoda iz InvoiceDao-a

--- XWS-REST/src/rs/ac/uns/ftn/xws/services/RestServerRemote.java
+++ XWS-REST/src/rs/ac/uns/ftn/xws/services/RestServerRemote.java
@@ -1,21 +1,27 @@
 package rs.ac.uns.ftn.xws.services;
 
+import java.net.URISyntaxException;
 import java.util.List;
+
+import javax.ws.rs.core.Response;
 
 import rs.ac.uns.ftn.xws.entities.payments.Invoice;
 import rs.ac.uns.ftn.xws.entities.payments.InvoiceItem;
+import xml.project.faktura.Faktura.StavkaFakture;
 
 public interface RestServerRemote {
 
 	public Invoice pribaviFakturu(long id_dobavljaca, long id_fakture);
 	public List<Invoice> pribaviFakture(long id_dobavljaca);
-	public void novaStavka(long id_dobavljaca, long id_fakture);
 	public List<InvoiceItem> pribaviStavke(long id_dobavljaca, long id_fakture);
-	public void izmeniStavke(long id_dobavljaca, long id_fakture, long redni_broj);
-	public void obrisiStavku(long id_dobavljaca, long id_fakture, long redni_broj);
+	public Response obrisiStavku(String id_dobavljaca, long id_fakture, long redni_broj);
 	public void slanjeFakture(long id_dobavljaca);
-	public InvoiceItem pribaviStavku(long id_dobavljaca, long id_fakture,
+	public Response pribaviStavku(String id_dobavljaca, long id_fakture,
 			long redni_broj);
+	public Response novaStavka(String id_dobavljaca, long id_fakture,
+			StavkaFakture newInvoiceItem) throws URISyntaxException;
+	Response izmeniStavke(String id_dobavljaca, long id_fakture,
+			long redni_broj, StavkaFakture newInvoiceItem);
 	
 	
 	
