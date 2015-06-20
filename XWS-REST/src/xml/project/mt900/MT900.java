d8d7367223898756f6990d6ffc3f2dc2529cb625
= <enjakovic@gmail.com>
2015-06-20 15:32:34 +0200

    Izbacen project dependency od rest projekta ka soap projektu. Update where necessary

---
+++ XWS-REST/src/xml/project/mt900/MT900.java
@@ -0,0 +1,250 @@
+
+package xml.project.mt900;
+
+import java.math.BigDecimal;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.basex.rest.Identifiable;
+
+import xml.project.globals.TBanke;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="ID_poruke" type="{http://www.project.xml/globals}ID_poruke"/>
+ *         &lt;element name="Banka_Duznik" type="{http://www.project.xml/globals}TBanke"/>
+ *         &lt;element name="Datum_Valute" type="{http://www.w3.org/2001/XMLSchema}date"/>
+ *         &lt;element name="Iznos" type="{http://www.project.xml/globals}TIznos"/>
+ *         &lt;element name="Sifra_valute" type="{http://www.project.xml/globals}TOznakaValute"/>
+ *         &lt;element name="Datum" type="{http://www.w3.org/2001/XMLSchema}date"/>
+ *         &lt;element name="ID_poruke_naloga" type="{http://www.project.xml/globals}ID_poruke"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "idPoruke",
+    "bankaDuznik",
+    "datumValute",
+    "iznos",
+    "sifraValute",
+    "datum",
+    "idPorukeNaloga"
+})
+@XmlRootElement(name = "MT900")
+public class MT900 extends Identifiable{
+
+    @XmlElement(name = "ID_poruke", required = true)
+    protected String idPoruke;
+    @XmlElement(name = "Banka_Duznik", required = true)
+    protected TBanke bankaDuznik;
+    @XmlElement(name = "Datum_Valute", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar datumValute;
+    @XmlElement(name = "Iznos", required = true)
+    protected BigDecimal iznos;
+    @XmlElement(name = "Sifra_valute", required = true)
+    protected String sifraValute;
+    @XmlElement(name = "Datum", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar datum;
+    @XmlElement(name = "ID_poruke_naloga", required = true)
+    protected String idPorukeNaloga;
+
+    /**
+     * Gets the value of the idPoruke property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIDPoruke() {
+        return idPoruke;
+    }
+
+    /**
+     * Sets the value of the idPoruke property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIDPoruke(String value) {
+        this.idPoruke = value;
+    }
+
+    /**
+     * Gets the value of the bankaDuznik property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link TBanke }
+     *     
+     */
+    public TBanke getBankaDuznik() {
+        return bankaDuznik;
+    }
+
+    /**
+     * Sets the value of the bankaDuznik property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link TBanke }
+     *     
+     */
+    public void setBankaDuznik(TBanke value) {
+        this.bankaDuznik = value;
+    }
+
+    /**
+     * Gets the value of the datumValute property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDatumValute() {
+        return datumValute;
+    }
+
+    /**
+     * Sets the value of the datumValute property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDatumValute(XMLGregorianCalendar value) {
+        this.datumValute = value;
+    }
+
+    /**
+     * Gets the value of the iznos property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getIznos() {
+        return iznos;
+    }
+
+    /**
+     * Sets the value of the iznos property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setIznos(BigDecimal value) {
+        this.iznos = value;
+    }
+
+    /**
+     * Gets the value of the sifraValute property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSifraValute() {
+        return sifraValute;
+    }
+
+    /**
+     * Sets the value of the sifraValute property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSifraValute(String value) {
+        this.sifraValute = value;
+    }
+
+    /**
+     * Gets the value of the datum property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDatum() {
+        return datum;
+    }
+
+    /**
+     * Sets the value of the datum property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDatum(XMLGregorianCalendar value) {
+        this.datum = value;
+    }
+
+    /**
+     * Gets the value of the idPorukeNaloga property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getIDPorukeNaloga() {
+        return idPorukeNaloga;
+    }
+
+    /**
+     * Sets the value of the idPorukeNaloga property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setIDPorukeNaloga(String value) {
+        this.idPorukeNaloga = value;
+    }
+	@Override
+	public Long getId() {
+		return Long.valueOf(getIDPoruke());
+	}
+
+	@Override
+	public void setId(Long id) {
+		setIDPoruke(String.valueOf(id));
+	}
+}
